8ad108aa11f944d9e3ae2b6179a0c6bbb283a174
llsrrll96 <58140426+llsrrll96@users.noreply.github.com>
2020-09-22 18:16:56 +0900

    로그인 - 회원가입 - 메인페이지 화면 전환

---
+++ app/src/main/java/com/example/emptytherefrigerator/main/activity_memberView.java
@@ -0,0 +1,29 @@
+package com.example.emptytherefrigerator.main;
+
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.emptytherefrigerator.R;
+
+public class activity_memberView extends AppCompatActivity
+{
+    ImageButton memberEditBtn, recipeInquireBtn, commentInquireBtn, likeInquireBtn, settingBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.member_menu);
+        initBtn();
+    }
+
+    private void initBtn()
+    {
+        memberEditBtn = findViewById(R.id.memberEditBtn);
+        recipeInquireBtn=findViewById(R.id.recipeInquireBtn);
+        commentInquireBtn = findViewById(R.id.commentInquireBtn);
+        likeInquireBtn = findViewById(R.id.likeInquireBtn);
+        settingBtn=findViewById(R.id.settingBtn);
+    }
+}
