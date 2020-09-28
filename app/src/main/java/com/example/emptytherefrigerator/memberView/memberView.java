8d83208103d1c77bc817124ba075ff392b2adb12
jaehe <jaehe0413@naver.com>
2020-09-28 20:41:44 +0900

    함수이름 수정

--- app/src/main/java/com/example/emptytherefrigerator/memberView/memberView.java
+++ app/src/main/java/com/example/emptytherefrigerator/memberView/memberView.java
@@ -1,6 +1,7 @@
 package com.example.emptytherefrigerator.memberView;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,21 +10,28 @@ import com.example.emptytherefrigerator.R;
 
 public class memberView extends AppCompatActivity
 {
-    ImageButton memberEditBtn, recipeInquireBtn, commentInquireBtn, likeInquireBtn, settingBtn;
+    private ImageButton memberEditBtn, recipeInquireBtn, commentInquireBtn, likeInquireBtn, settingBtn;
+    private Button logoutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.member_menu);
-        initBtn();
+        initializeView();
     }
 
-    private void initBtn()
+    private void initializeView()
     {
         memberEditBtn = findViewById(R.id.memberEditBtn);
         recipeInquireBtn=findViewById(R.id.recipeInquireBtn);
         commentInquireBtn = findViewById(R.id.commentInquireBtn);
         likeInquireBtn = findViewById(R.id.likeInquireBtn);
         settingBtn=findViewById(R.id.settingBtn);
+
+        logoutBtn= findViewById(R.id.logoutBtn);
+    }
+    private void setListener()
+    {
+
     }
 }
