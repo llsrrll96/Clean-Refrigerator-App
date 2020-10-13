5f415ab34a461a9bbc52a4a4b1f4a64c1e695478
jaehe <jaehe0413@naver.com>
2020-10-13 22:24:23 +0900

    데이터 받는 형식 수정
    html -> json으로 받는 데이터의 형식을 수정했다

--- app/src/main/java/com/example/emptytherefrigerator/network/NetworkHandler.java
+++ app/src/main/java/com/example/emptytherefrigerator/network/NetworkHandler.java
@@ -31,7 +31,7 @@ public class NetworkHandler
             conn.setRequestMethod("POST");//POST방식으로 보냄
             conn.setRequestProperty("Cache-Control", "no-cache");//캐시 설정
             conn.setRequestProperty("Content-Type", "application/json");//application JSON 형식으로 전송
-            conn.setRequestProperty("Accept", "text/html"); //서버에 response 데이터를 html로 받음
+            conn.setRequestProperty("Accept", "application/json");  //받을 데이터도 json 형식으로 받음
 
             conn.setDoOutput(true);//Outstream으로 post 데이터를 넘겨주겠다는 의미
             conn.setDoInput(true);//Inputstream으로 서버로부터 응답을 받겠다는 의미
@@ -60,14 +60,17 @@ public class NetworkHandler
         StringBuffer resultString = new StringBuffer();
         try
         {
+
             outStream = conn.getOutputStream();
             //버퍼를 생성하고 넣음
             writer = new BufferedWriter(new OutputStreamWriter(outStream));
 
-            writer.write(jsonObject);        //json data를 보내기 위해 string으로 변환
-            writer.flush();
-            writer.close();//버퍼를 닫음
-
+            if(jsonObject!=null)        //보낼 데이터가 없는 경우가 있을때만
+            {
+                writer.write(jsonObject);
+                writer.flush();
+                writer.close();//버퍼를 닫음
+            }
             inputStream = conn.getInputStream();      //서버로부터 데이터를 받음
             reader = new BufferedReader(new InputStreamReader(inputStream));
             resultString = new StringBuffer();
