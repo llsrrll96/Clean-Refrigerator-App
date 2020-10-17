6c15ff224abf09aa0f34bf228490499a20c5a37d
km7865 <51434817+km7865@users.noreply.github.com>
2020-10-17 10:12:04 +0900

    레시피 관리 AsyncTask 구현

--- app/src/main/java/com/example/emptytherefrigerator/AsyncTasks/RecipeMngAsyncTask.java
+++ app/src/main/java/com/example/emptytherefrigerator/AsyncTasks/RecipeMngAsyncTask.java
@@ -2,18 +2,30 @@ package com.example.emptytherefrigerator.AsyncTasks;
 
 import android.os.AsyncTask;
 
+import com.example.emptytherefrigerator.network.NetworkHandler;
+
 //doinbackground 변수 타입, onProgress 타입, onPostExcute 타입
 public class RecipeMngAsyncTask extends AsyncTask<String, Integer, String>
 {
     int values = 0;
 
     @Override
-    protected String doInBackground(String... urls) {
-//        switch(urls[0])
-//        {
-//
-//        }
-        return "";
+    protected String doInBackground(String... strings) {
+        String functionType = strings[0];
+        String data = strings[1];
+        switch (functionType)
+        {
+            case "createRecipe" : break;
+            case "updateRecipe" : break;
+            case "deleteRecipe" : break;
+            case "readRecipe" : break;
+        }
+        
+        NetworkHandler.getInstance().connect(functionType);      // 서버와 연결
+        String result = NetworkHandler.communication(data);
+        NetworkHandler.disconnect();
+
+        return result;
     }
 
     //백그라운드 스레드에서 작업 처리 중에 프로그레스바 진행 상태 업데이트 같은  UI작업이 필요한 경우
