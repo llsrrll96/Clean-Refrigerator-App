0e5b660063aec79c62d34812f6e4a7c6c3a9b4a5
jaehe <jaehe0413@naver.com>
2020-10-18 14:00:54 +0900

    회원창 이름 바꿈

---
+++ app/src/main/java/com/example/emptytherefrigerator/memberView/UserView.java
@@ -0,0 +1,93 @@
+package com.example.emptytherefrigerator.memberView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.emptytherefrigerator.R;
+
+//Main 화면 Home
+//이 달의 레시피 검색 결과
+public class UserView extends Fragment
+{
+    private ImageButton btnEditUser, btnRecipeInquire, btnCommentInquire, btnLIkeInquire, btnSetting;
+    private Button logoutBtn;
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        view = inflater.inflate(R.layout.member_menu,container,false);
+        initializeView();
+        return view;
+    }
+    private void initializeView()
+    {
+        btnEditUser = view.findViewById(R.id.memberEditBtn);
+        btnRecipeInquire = view.findViewById(R.id.recipeInquireBtn);
+        btnCommentInquire = view.findViewById(R.id.commentInquireBtn);
+        btnLIkeInquire = view.findViewById(R.id.likeInquireBtn);
+        btnSetting=view.findViewById(R.id.settingBtn);
+        logoutBtn= view.findViewById(R.id.logoutBtn);
+    }
+    private void setListener()
+    {
+        btnEditUser.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    //멤버 수정 화면
+                }
+            });
+        btnRecipeInquire.setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        //내 레시피 조회
+                    }
+                });
+        btnCommentInquire.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //내 댓글 조회
+            }
+        });
+        btnLIkeInquire.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //내 좋아요 조회
+            }
+        });
+        btnSetting.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //셋팅 화면 넘김
+            }
+        });
+        logoutBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //로그아웃
+            }
+        });
+    }
+}
