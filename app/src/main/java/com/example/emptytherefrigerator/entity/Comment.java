a5aa8767e5c856445e5030f0087c8247de532dac
jaehe <jaehe0413@naver.com>
2020-10-19 02:11:50 +0900

    내 댓글조회 구현
    화면 넘기기까지 완료

---
+++ app/src/main/java/com/example/emptytherefrigerator/entity/Comment.java
@@ -0,0 +1,38 @@
+package com.example.emptytherefrigerator.entity;
+
+public class Comment
+{
+    private int recipeId;
+    private int commentId;
+    private String recipeTitle;
+    private String userId;  //댓글 등록자 id
+    private String content; //댓글 내용
+    private String uploadDate;  //댓글 작성 날짜
+
+    public Comment()
+    {
+
+    }
+    public Comment(int recipeId, int commentId, String recipeTitle, String userId, String content, String uploadDate)
+    {
+        this.recipeId = recipeId;
+        this.commentId = commentId;
+        this.recipeTitle = recipeTitle;
+        this.userId = userId;
+        this.content = content;
+        this.uploadDate = uploadDate;
+    }
+
+    public int getRecipeId() {return recipeId;}
+    public void setRecipeId(int recipeId) {this.recipeId = recipeId;}
+    public int getCommentId() {return commentId;}
+    public void setCommentId(int commentId) {this.commentId = commentId;}
+    public String getRecipeTitle() {return recipeTitle;}
+    public void setRecipeTitle(String recipeTitle) {this.recipeTitle = recipeTitle;}
+    public String getUserId() {return userId;}
+    public void setUserId(String userId) {this.userId = userId;}
+    public String getContent() {return content;}
+    public void setContent(String content) {this.content = content;}
+    public String getUploadDate() {return uploadDate;}
+    public void setUploadDate(String uploadDate) {this.uploadDate = uploadDate;}
+}
---
+++ app/src/main/java/com/example/emptytherefrigerator/memberView/MyComment/MyCommentAdapter.java
@@ -0,0 +1,90 @@
+package com.example.emptytherefrigerator.memberView.MyComment;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.emptytherefrigerator.R;
+import com.example.emptytherefrigerator.entity.Comment;
+import com.example.emptytherefrigerator.entity.Recipe;
+import com.example.emptytherefrigerator.memberView.MyRecipe.MyRecipeListAdapter;
+
+import java.util.ArrayList;
+
+public class MyCommentAdapter extends RecyclerView.Adapter<MyCommentAdapter.MyCommentViewHolder>
+{
+    private ArrayList<Comment> list;
+    private LayoutInflater inflater;
+
+    public MyCommentAdapter(Context context, ArrayList<Comment> list)
+    {
+        inflater = LayoutInflater.from(context);
+        this.list = list;
+    }
+    @Override
+    public void onBindViewHolder(@NonNull MyCommentAdapter.MyCommentViewHolder holder, int position)
+    {
+        holder.onBind(list.get(position));
+    }
+    @Override
+    public int getItemCount()       //전체 아이템 갯수 리턴
+    {
+        return list.size();
+    }
+
+    @Override
+    public MyCommentViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    {
+        View itemView = inflater.inflate(R.layout.member_my_comment_item, parent, false);
+        return new MyCommentAdapter.MyCommentViewHolder(itemView, this);
+    }
+
+    class MyCommentViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
+    {
+        MyCommentAdapter adapter;
+        TextView commentListTitle, myCommentContent, myCommentUploadDate;
+        ImageView myComment_RecipeMainImage;
+        ImageButton btnCommentDel;
+
+        public MyCommentViewHolder(View itemView, MyCommentAdapter adapter)
+        {
+            super(itemView);
+            this.adapter = adapter;
+            commentListTitle = itemView.findViewById(R.id.commentListTitle);
+            myCommentContent = itemView.findViewById(R.id.myCommentContent);
+            myCommentUploadDate = itemView.findViewById(R.id.myCommentUploadDate);
+            myComment_RecipeMainImage = itemView.findViewById(R.id.myComment_RecipeMainImage);
+            btnCommentDel = itemView.findViewById(R.id.btnCommentDel);
+            setListener();
+        }
+        public void onBind(Comment comment)
+        {
+            commentListTitle.setText(comment.getRecipeTitle());
+            myCommentContent.setText(comment.getUserId());
+            myCommentUploadDate.setText(comment.getUploadDate());
+            //이미지 설정
+        }
+        public void setListener()
+        {
+            btnCommentDel.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v)
+                {
+                    //댓글 삭제
+                }
+            });
+        }
+        @Override
+        public void onClick(View v)
+        {
+            //조회 화면 넘기기
+        }
+    }
+}
