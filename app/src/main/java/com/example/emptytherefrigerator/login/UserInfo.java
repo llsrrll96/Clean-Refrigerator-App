511330d2b4e883778044c06bf941bc74fc8a96fb
jaehe <jaehe0413@naver.com>
2020-10-24 21:23:49 +0900

    sharedPreference 관련
    sharedPreference를 extend한건 아니고 키 값을 편하게 가져오고, 정리해두려고 따로 클래스에서 값을 보관하도록 함

---
+++ app/src/main/java/com/example/emptytherefrigerator/login/UserInfo.java
@@ -0,0 +1,10 @@
+package com.example.emptytherefrigerator.login;
+
+public class UserInfo           //자동 로그인 밑 앱 내에서 사용자의 정보를 저장하기 위해 sharedpreference 활용
+{
+    public static final String PREFERENCES_NAME = "come.example.emptytherefrigerator.userInfo";
+
+    public static final String ID_KEY = "USER_ID";
+    public static final String PW_KEY = "USER_PW";
+
+}
