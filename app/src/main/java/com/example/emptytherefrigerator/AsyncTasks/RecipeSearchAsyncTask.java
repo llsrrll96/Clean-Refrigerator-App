b599f71cb2ba548f48b6911fdf8568bc448187ff
jaehe <jaehe0413@naver.com>
2020-10-25 15:49:11 +0900

    누락됐던거 다시 추가

--- app/src/main/java/com/example/emptytherefrigerator/AsyncTasks/RecipeSearchAsyncTask.java
+++ app/src/main/java/com/example/emptytherefrigerator/AsyncTasks/RecipeSearchAsyncTask.java
@@ -2,23 +2,21 @@ package com.example.emptytherefrigerator.AsyncTasks;
 
 import android.os.AsyncTask;
 import com.example.emptytherefrigerator.entity.RecipeIn;
-import com.example.emptytherefrigerator.network.JsonParsing;
 import com.example.emptytherefrigerator.network.NetworkHandler;
 
 import java.util.ArrayList;
 
 //doinbackground 변수 타입, onProgress 타입, onPostExcute 타입
-public class RecipeSearchAsyncTask extends AsyncTask<String, Void, ArrayList<RecipeIn>>
+public class RecipeSearchAsyncTask extends AsyncTask<String, Void, String>
 {
-    protected ArrayList<RecipeIn> doInBackground(String... strings)
+    protected String doInBackground(String... strings)
     {
         ArrayList<RecipeIn> recipeList = new ArrayList<RecipeIn>();
 
         NetworkHandler.getInstance().connect(strings[0]);      //서버와 연결
         String jsonData = NetworkHandler.communication(null);        //요청만 하기 때문에 보낼 데이터는 없음, result는 null
-        recipeList = JsonParsing.parsingRecipe(jsonData);
         NetworkHandler.disconnect();
 
-        return recipeList;
+        return jsonData;
     }
 }
--- app/src/main/java/com/example/emptytherefrigerator/network/JsonParsing.java
+++ app/src/main/java/com/example/emptytherefrigerator/network/JsonParsing.java
@@ -1,10 +1,12 @@
 package com.example.emptytherefrigerator.network;
 
+import com.example.emptytherefrigerator.entity.RecipeComment;
 import com.example.emptytherefrigerator.entity.RecipeIn;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class JsonParsing
@@ -22,7 +24,7 @@ public class JsonParsing
             for(int i=0; i<resRecipeList.length(); i++)
             {
                 JSONObject recipe = resRecipeList.getJSONObject(i);
-                String recipeId = recipe.getString("recipeId");
+                int recipeId = recipe.getInt("recipeId");
                 String title = recipe.getString("title");
                 String userId = recipe.getString("userId");
                 String ingredient = recipe.getString("ingredient");
@@ -36,6 +38,36 @@ public class JsonParsing
                 String uploadDate = recipe.getString("uploadData");
 
                 list.add(new RecipeIn(recipeId, title, userId, ingredient, recipePerson, recipeTime, recipeImagePath, recipeContents, commentCount, likeCount, uploadDate));
+            }
+        }
+        catch(JSONException e)
+        {
+            e.printStackTrace();
+        }
+        return list;
+    }
+    public static ArrayList<RecipeComment>parsingCommentList(String data)
+    {
+        ArrayList<RecipeComment> list = new ArrayList<>();
+        JSONObject jsonObject;
+        try
+        {
+            jsonObject = new JSONObject(data);
+            String jsonString = jsonObject.getString("myCommentList");
+            JSONArray resCommentList = new JSONArray(jsonString);
+
+            for(int i=0; i<resCommentList.length(); i++)
+            {
+                JSONObject comment = resCommentList.getJSONObject(i);
+
+                int recipeId = comment.getInt("recipeId");
+                int commentId = comment.getInt("commentId");
+                String title = comment.getString("title");
+                String userId = comment.getString("userId");
+                String content = comment.getString("content");
+                String uploadDate = comment.getString("uploadData");
+
+                list.add(new RecipeComment(recipeId, commentId, title, userId, content, uploadDate));
             }
         }
         catch(JSONException e)
